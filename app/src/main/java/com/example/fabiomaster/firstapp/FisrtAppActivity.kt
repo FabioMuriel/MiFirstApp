d5835a2a1b4379c81e772ed19068c66205ea4d58
Fabio Muriel <fabiomurielvega16@gmail.com>
2023-11-12 23:29:30 -0500

    SaluApp

--- app/src/main/java/com/example/fabiomaster/firstapp/FisrtAppActivity.kt
+++ app/src/main/java/com/example/fabiomaster/firstapp/FisrtAppActivity.kt
@@ -1,12 +1,27 @@
 package com.example.fabiomaster.firstapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fabiomaster.R
 
 class FisrtAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fisrt_app)
+        val btnClick: Button = findViewById<Button>(R.id.btnClick);
+        val etName: EditText = findViewById<EditText>(R.id.etName);
+        btnClick.setOnClickListener {
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent);
+            }
+        }
     }
 }
